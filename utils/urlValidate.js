b3576973e1d64881a0ea75b771776549fb432a1a
latypovroman <jiggawhat5@gmail.com>
2022-05-30 00:53:29 +0500

    fixed validation error

--- utils/urlValidate.js
+++ utils/urlValidate.js
@@ -1,7 +1,7 @@
 const { isURL } = require('validator');
 const BadRequestError = require('../errors/BadRequestError');
 
-module.exports.method = (value) => {
+module.exports.urlValidate = (value) => {
   const result = isURL(value);
   if (result) {
     return value;
